8faa7087b2a07c1d35cef72c03c6dc6b0a1fbfec
gyannn1 <deenggian@gmail.com>
2025-04-17 22:11:31 -0700

    Add files via upload

---
+++ logika.py
@@ -0,0 +1,39 @@
+x = True
+y = True
+nilai = x and y
+print(x,"and",y,"=",nilai)
+
+x = True
+y = False
+nilai = x and y
+print(x,"and",y,"=",nilai)
+
+x = False
+y = True
+nilai = x and y
+print(x,"and",y,"=",nilai)
+
+x = False
+y = False
+nilai = x and y
+print(x,"and",y,"=",nilai)
+
+x = True
+y = True
+nilai = x ^ y
+print(x,"or",y,"=",nilai)
+
+x = True
+y = False
+nilai = x ^ y
+print(x,"or",y,"=",nilai)
+
+x = False
+y = True
+nilai = x ^ y
+print(x,"or",y,"=",nilai)
+
+x = False
+y = False
+nilai = x ^ y
+print(x,"or",y,"=",nilai)
